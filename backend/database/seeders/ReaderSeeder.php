ce52717cac48e10bbb8da5ccbb193f02a232ec4b
Victor Nogueira <victornogu80@gmail.com>
2022-05-01 19:41:34 -0300

    Criação de seeders com o login do Cypress para testes e2e no VUE

--- backend/database/seeders/ReaderSeeder.php
+++ backend/database/seeders/ReaderSeeder.php
@@ -16,13 +16,27 @@ class ReaderSeeder extends Seeder
      */
     public function run()
     {
-        return  Reader::create([
-            'name' => 'Teste',
-            'email' => 'teste@teste.com',
-            'password' => Hash::make('teste'),
-            'phone' => '(41) 99999-9999',
-            'birthday' => '1985-12-12',
-            'address' => 'Rua Teste, 12',
-        ]);
+        $readers = [
+            [
+                'name' => 'Cypress',
+                'email' => 'cypress@cypress.com.br',
+                'password' => Hash::make('teste'),
+                'phone' => '(41) 99999-9999',
+                'birthday' => '1985-12-12',
+                'address' => 'Rua Teste, 12',
+            ],
+            [
+                'name' => 'Teste',
+                'email' => 'teste@teste.com',
+                'password' => Hash::make('teste'),
+                'phone' => '(41) 99999-9999',
+                'birthday' => '1985-12-12',
+                'address' => 'Rua Teste, 12',
+            ]
+        ];
+
+        foreach ($readers as $reader) {
+            Reader::create($reader);
+        }
     }
 }
